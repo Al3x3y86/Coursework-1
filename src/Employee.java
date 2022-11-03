9f51959ef79c6735613b50d684112f8887ea5c41
Alexey <alexey_kuzminykh@mail.ru>
2022-11-03 11:30:46 +0300

    Base difficulty

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,69 @@
 public class Employee {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private double salary;
+    private int department;
+    private final int id;
+    public static int counter = 1;
+
+    public Employee(String firstName, String middleName, String lastName, double salary, int department) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.department = department;
+        this.id = counter++;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public double getSalary() {
+        return salary;
+    }
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName(Employee[] employee) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        return firstName + "" + middleName + "" + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return  "ФИО: " + firstName + middleName + lastName +
+                ", Зарплата: " + salary +
+                ", Отдел: " + department +
+                ", ID: " + id;
+    }
 }
--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,2 +1,58 @@
 public class EmployeeBook {
+    public static void leadOutListEmployees(Employee[] employee) {
+        for (byte i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                System.out.println(employee[i].toString());
+            }
+        }
+    }
+
+    public static double calculationTotalSalaries(Employee[] employee) {
+        double totalSalary = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                totalSalary = totalSalary + employee[i].getSalary();
+            }
+        }
+        System.out.println("Сумма затрат на зарплаты в месяц: "+totalSalary);
+        return totalSalary;
+    }
+
+    public static void minSalary(Employee[] employee) {
+        double min = employee[0].getSalary();
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                if (min > employee[i].getSalary()) {
+                    min = employee[i].getSalary();
+                }
+            }
+        }
+        System.out.println("Минимальная зарплата: "+min);
+    }
+
+    public static void maxSalary(Employee[] employee) {
+        double max = employee[0].getSalary();
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                if (max < employee[i].getSalary()) {
+                    max = employee[i].getSalary();
+                }
+            }
+        }
+        System.out.println("Максимальная зарплата: "+max);
+    }
+
+    public static void averageSalary(Employee[] employee) {
+        double mid = calculationTotalSalaries(employee) / employee.length;
+        System.out.println("Cреднее значение зарплат: "+ mid);
+    }
+
+    public static void leadOutNamesEmployees(Employee[] employee) {
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                System.out.println(employee[i].getFullName(employee));
+            }
+        }
+    }
 }
+
